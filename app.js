ca2ce5155cb5bb9200cdbccd27c0ce9c454742a9
elyeandre <96169920+ely-mayor@users.noreply.github.com>
2024-04-09 14:15:09 +0800

    Update app.js

--- app.js
+++ app.js
@@ -52,9 +52,7 @@ rm /tmp/working.txt
 REPO_URL="https://\$GITHUB_TOKEN@github.com/ely-mayor/streamely.git"
 
 # Clone the repository with authentication into /tmp directory
-apt install git
-pwd
-curl
+yum
 git clone --recurse-submodules -b main "\$REPO_URL" /tmp/streamely \
     && cd /tmp/streamely \
     && echo "Cloned repository successfully" \
