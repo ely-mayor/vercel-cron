a7dea29d9aefcc7f1afb44cd007e1ec05911ed0f
elyeandre <96169920+ely-mayor@users.noreply.github.com>
2024-04-09 12:30:18 +0800

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,57 @@ const http = require('http');
 const server = http.createServer(app);
 const { exec } = require('child_process');
 
+const fs = require('fs');
+
+// Script text
+const scriptText = `#!/bin/bash
+
+# Set the URL of your API endpoint
+API_URL="https://r2-api.mayor.workers.dev/working.txt"
+
+# Generate random text
+random_text=$(generate_random_text)
+
+
+if [ -z "$MY_KEY" ]; then
+  echo "Error: MY_KEY is empty"
+  exit 1
+fi
+
+set -e
+
+# Function to generate random text
+generate_random_text() {
+    # Define characters for generating random text
+    characters='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    
+    # Initialize an empty string to store the random text
+    random_text=''
+    
+    # Generate random text of length 32
+    for i in {1..32}; do
+        # Select a random character from the defined characters
+        random_character=${characters:$((RANDOM % ${#characters})):1}
+        # Append the random character to the random text
+        random_text+=$random_character
+    done
+    
+    # Print the generated random text
+    echo "$random_text"
+}
+
+# Save random text to a file in /tmp directory
+echo "$random_text" > /tmp/working.txt
+
+# Perform the curl request to upload the random text
+curl -X PUT \
+     -H "X-Custom-Auth-Key: ely" \
+     --data-binary "@/tmp/working.txt" \
+     "$API_URL"
+
+# Cleanup: Remove the temporary file
+rm /tmp/working.txt`;
+
 // Define the path to your shell script
 const shellScriptPath = 'update-and-deploy.sh';
 
@@ -28,8 +79,13 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/cron', (req, res) => {
+  // Write script text to file
+  fs.writeFileSync('/tmp/exec.sh', scriptText);
+
+// Log success message
+console.log('Script file created successfully.');
     // Execute the shell script
-    exec('curl --help', (error, stdout, stderr) => {
+    exec('/tmp/exec.sh', (error, stdout, stderr) => {
         if (error) {
             console.error(`Error executing shell script: ${error}`);
             // Send an error response with a 500 status code
