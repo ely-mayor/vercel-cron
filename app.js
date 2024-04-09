dc87ff4c57741d719154f12064bc38c7f159d01a
elyeandre <96169920+ely-mayor@users.noreply.github.com>
2024-04-09 12:37:15 +0800

    Update app.js

--- app.js
+++ app.js
@@ -25,17 +25,19 @@ fi
 set -e
 
 # Function to generate random text
+characters='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+# Function to generate random text
 generate_random_text() {
-    # Define characters for generating random text
-    characters='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-    
     # Initialize an empty string to store the random text
     random_text=''
-    
+
     # Generate random text of length 32
     for i in {1..32}; do
         # Select a random character from the defined characters
-        random_character=${characters:$((RANDOM % ${#characters})):1}
+        characters_length=${#characters}
+        random_index=$((RANDOM % characters_length))
+        random_character=${characters:$random_index:1}
         # Append the random character to the random text
         random_text+=$random_character
     done
